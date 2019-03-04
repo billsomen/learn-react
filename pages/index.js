9f35f8f79ec982f71d0e3e3b1a8dd411d22d528f
Somen Diego <ngongangsomen@gmail.com>
2019-03-04 01:43:50 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 import Layout from "../components/ui/Layout";
 import fetch from "isomorphic-unfetch";
 
-const Index = (props) => (
+const Index = props => (
   <Layout>
     <h1>Batman TV Shows</h1>
     <ul>
-      {props.shows.map(({show}) => (
+      {props.shows.map(({ show }) => (
         <li key={show.id}>
           <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
             <a>{show.name}</a>
--- pages/post.js
+++ pages/post.js
@@ -1,22 +1,22 @@
 import Layout from "../components/ui/Layout";
 import fetch from "isomorphic-unfetch";
 
-const Post = (props) => (
+const Post = props => (
   <Layout>
     <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <p>{props.show.summary.replace(/<[/]?p>/g, "")}</p>
     <img src={props.show.image.medium} alt={`${props.show.name} image`} />
   </Layout>
 );
 
-Post.getInitialProps = async function (context) {
-  const {id} = context.query;
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
   const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
   const show = await res.json();
 
   console.log(`Fetched show: ${show.name}`);
 
-  return {show};
+  return { show };
 };
 
 export default Post;
